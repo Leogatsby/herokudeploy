196e58f19590514a8eae95bbd945b51a1ff8f724
leo <100k1@naver.com>
2021-10-27 21:28:55 +0900

    이제는 제대로 되겠지? redirect 주소 추가함

--- routes/deploytestRouter.js
+++ routes/deploytestRouter.js
@@ -2,6 +2,10 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
+router.get('/', (req,res) =>{
+    res.redirect('/main')
+})
+
 router.get('/main', (req,res) =>{
     res.sendFile(path.join(__dirname, '../', 'views' , 'main.html'));
 })
